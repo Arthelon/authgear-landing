d761003f1547e404c6a98863d52affaa0b08508e
Arthelon <hsing.daniel@gmail.com>
2020-05-21 11:44:17 +0800

    Initialize HeroHeader component with header image and tagline
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

---
+++ src/components/HeroHeader.svelte
@@ -0,0 +1,67 @@
+<style>
+  section {
+    background-color: #fff;
+    padding: 164px 0 144px 0;
+    color: #000;
+  }
+
+  .container-fluid {
+    max-width: 75rem;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  .tagline h1 {
+    font-weight: 900;
+  }
+
+  .tagline p {
+    font-size: 18px;
+    font-weight: 400;
+    margin-bottom: 52px;
+  }
+
+  @media screen and (min-width: 768px) {
+    .container-fluid {
+      padding-right: 3.5rem;
+      padding-left: 3.5rem;
+    }
+
+    .tagline h1 {
+      margin-bottom: 16px;
+      font-size: 42px;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .container-fluid {
+      padding-right: 1.8rem;
+      padding-left: 1.8rem;
+    }
+
+    .tagline h1 {
+      margin-bottom: 7px;
+      font-size: 28px;
+    }
+  }
+</style>
+
+<section>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 col-md-5 t tagline">
+        <h1>Don't Compromise Security, Let us help</h1>
+        <p>
+          Rapidly integrate authentication for your web and mobile applications,
+          with security and reliability
+        </p>
+      </div>
+      <div class="col-12 col-md-7">
+        <img src="hero-header-img.png" alt="Hero Header" />
+      </div>
+    </div>
+  </div>
+</section>
