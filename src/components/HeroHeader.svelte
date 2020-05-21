a647fa1f05bac04e2a144757b30d27e31c49a825
Arthelon <hsing.daniel@gmail.com>
2020-05-21 12:28:56 +0800

    Add placeholder contact input and button
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/components/HeroHeader.svelte
+++ src/components/HeroHeader.svelte
@@ -24,6 +24,34 @@
     margin-bottom: 52px;
   }
 
+  .contact-wrapper {
+    display: flex;
+  }
+
+  .contact-wrapper input {
+    outline: none;
+    border: none;
+    border-bottom: 1px solid #979797;
+    padding-bottom: 10px;
+  }
+
+  .contact-wrapper button {
+    color: #fff;
+    cursor: pointer;
+    height: fit-content;
+    width: fit-content;
+    padding: 10px 24px;
+    background: #166ff7;
+    outline: none;
+    border: none;
+    border-radius: 2px;
+    box-shadow: 0 2px 7px hsla(0, 0%, 0%, 0.5);
+  }
+
+  .contact-wrapper button:hover {
+    color: #1468a0;
+  }
+
   @media screen and (min-width: 768px) {
     .container-fluid {
       padding-right: 3.5rem;
@@ -33,6 +61,11 @@
     .tagline h1 {
       margin-bottom: 16px;
       font-size: 42px;
+    }
+
+    .contact-wrapper input {
+      flex: 3 3;
+      margin-right: 20px;
     }
   }
 
@@ -46,6 +79,15 @@
       margin-bottom: 7px;
       font-size: 28px;
     }
+
+    .contact-wrapper {
+      flex-direction: column;
+    }
+
+    .contact-wrapper button {
+      margin-top: 13px;
+      margin-bottom: 30px;
+    }
   }
 </style>
 
@@ -58,6 +100,10 @@
           Rapidly integrate authentication for your web and mobile applications,
           with security and reliability
         </p>
+        <div class="contact-wrapper">
+          <input type="email" placeholder="Your Email" />
+          <button>Talk to Us</button>
+        </div>
       </div>
       <div class="col-12 col-md-7">
         <img src="hero-header-img.png" alt="Hero Header" />
