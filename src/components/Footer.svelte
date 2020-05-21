53f77f96050ea6e2d715cbcd9ddf3e5346e974ef
Arthelon <hsing.daniel@gmail.com>
2020-05-21 11:17:00 +0800

    Add placeholder footer links and footer copyright section
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,6 +3,9 @@
     padding-top: 50px;
     padding-bottom: 50px;
     background: #222;
+    color: #fff;
+    font-size: 12px;
+    font-weight: 400;
   }
 
   .container-fluid {
@@ -11,12 +14,49 @@
 
   img {
     max-width: 100px;
+    margin-bottom: 40px;
+  }
+
+  .sitemap-title {
+    text-transform: uppercase;
+    font-weight: 700;
+    letter-spacing: 1px;
+  }
+
+  .sitemap-list {
+    margin: 13px 0;
+    padding: 0;
+    list-style: none;
+    color: #aaaaaa;
+  }
+
+  a {
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  .copyright-wrapper {
+    margin-top: 60px;
   }
 
   @media screen and (min-width: 768px) {
     .container-fluid {
       padding-right: 2.8rem;
       padding-left: 2.8rem;
+    }
+    .copyright {
+      text-align: right;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .container-fluid {
+      padding-right: 1.8rem;
+      padding-left: 1.8rem;
+    }
+
+    .copyright {
+      text-align: left;
     }
   }
 </style>
@@ -27,13 +67,81 @@
       <div class="col-12 col-md-6">
         <img src="footer-logo.png" alt="Footer Logo" />
       </div>
-      <div class="col-12 col-md-2" />
-      <div class="col-12 col-md-2" />
-      <div class="col-12 col-md-2" />
+      <div class="col-12 col-md-2">
+        <span class="sitemap-title">Learn</span>
+        <ul class="sitemap-list">
+          <li>
+            <a>Blog</a>
+
+          </li>
+          <li>
+            <a>Demo Projects</a>
+          </li>
+          <li>
+            <a>Documentation</a>
+          </li>
+          <li>
+            <a>Tutorials</a>
+          </li>
+          <li>
+            <a>Terms of Services</a>
+          </li>
+          <li>
+            <a>Our Promises</a>
+          </li>
+          <li>
+            <a>Acceptable Use Policy</a>
+          </li>
+          <li>
+            <a>Data Privacy Policy</a>
+          </li>
+          <li>
+            <a>Service Level Agreement</a>
+          </li>
+          <li>
+            <a>Security</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-12 col-md-2">
+        <span class="sitemap-title">Company</span>
+        <ul class="sitemap-list">
+          <li>
+            <a>Team</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+          <li>
+            <a>Jobs</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-12 col-md-2">
+        <span class="sitemap-title">Connect</span>
+        <ul class="sitemap-list">
+          <li>
+            <a>Twitter</a>
+          </li>
+          <li>
+            <a>Facebook</a>
+          </li>
+          <li>
+            <a>Github</a>
+          </li>
+          <li>
+            <a>Discourse</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="row">
-      <div class="col-12 col-md-6" />
-      <div class="col-2 col-md-6" />
+    <div class="row copyright-wrapper">
+      <div class="col-12 col-md-6">
+        <a href="mailto:hello@skygear.io">hello@skygear.io</a>
+      </div>
+      <div class="col-12 col-md-6 copyright">
+        <span>© Oursky Ltd. All rights reserved</span>
+      </div>
     </div>
   </div>
 </footer>
