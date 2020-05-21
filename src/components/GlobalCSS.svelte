3220f33502f38a4c038ab6c5a480d5b860a757de
Arthelon <hsing.daniel@gmail.com>
2020-05-21 15:35:33 +0800

    Move font-face import into separate link tag
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/components/GlobalCSS.svelte
+++ src/components/GlobalCSS.svelte
@@ -2,8 +2,6 @@
   @import "../../node_modules/normalize.css/normalize.css";
   @import "../../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
 
-  @import url("https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;500;700;900&display=swap");
-
   body {
     font-family: "Red Hat Display", Roboto, -apple-system, BlinkMacSystemFont,
       Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
