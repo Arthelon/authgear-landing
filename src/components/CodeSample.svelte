541928ce37d4d98b7fc20d39e0101f81b6eb8781
Arthelon <hsing.daniel@gmail.com>
2020-05-22 13:02:03 +0800

    Initialize code sample section with desktop view, add placeholder UI demo image
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

---
+++ src/components/CodeSample.svelte
@@ -0,0 +1,63 @@
+<style>
+  .code-sample-section {
+    padding-top: 0;
+    padding-bottom: 80px;
+  }
+
+  .container-fluid {
+    max-width: 1200px;
+  }
+
+  .title {
+    color: #166ff7;
+    font-size: 32px;
+    text-align: center;
+    margin: 0;
+  }
+
+  .ui-img-wrapper {
+    text-align: center;
+    padding-top: 25px;
+  }
+
+  .ui-img {
+    max-width: 420px;
+    height: auto;
+    display: inline-block;
+    vertical-align: top;
+  }
+
+  @media screen and (min-width: 768px) {
+    .container-fluid {
+      padding-left: 56px;
+      padding-right: 56px;
+    }
+
+    .content-row {
+      margin-top: 92px;
+    }
+  }
+</style>
+
+<section class="code-sample-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="title">
+          Integrate Skygear Auth in Any Application in Just 5 Minutes!
+        </h1>
+      </div>
+    </div>
+    <div class="content-row row">
+      <div class="col-12 col-md-6" />
+      <div class="col-12 col-md-6">
+        <div class="ui-img-wrapper">
+          <img
+            class="ui-img"
+            src="ui-demo.png"
+            alt="UI Demo of Skygear Auth SSO" />
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
