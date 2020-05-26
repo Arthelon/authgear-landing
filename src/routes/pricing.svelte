00d2929dafaf87393fe1d1db2bbce1fc53ff48d2
Arthelon <hsing.daniel@gmail.com>
2020-05-26 11:43:40 +0800

    Add header to pricing page
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/routes/pricing.svelte
+++ src/routes/pricing.svelte
@@ -1,9 +1,10 @@
 <script>
+  import PricingHeader from "../components/PricingHeader.svelte";
   import Resources from "../components/Resources.svelte";
 </script>
 
 <svelte:head>
   <title>Pricing - Skygear Auth</title>
 </svelte:head>
-
+<PricingHeader />
 <Resources />
