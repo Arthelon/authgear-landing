561a7379a2ae2fe0d61c807bd92e1c58451db490
Arthelon <hsing.daniel@gmail.com>
2020-05-27 16:16:59 +0800

    Merge nav links together
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,7 +1,3 @@
-<script>
-  import MobileMenu from "./MobileMenu.svelte";
-</script>
-
 <style>
   .nav {
     position: fixed;
@@ -24,6 +20,13 @@
     width: 170px;
     border: 0;
     vertical-align: middle;
+  }
+
+  .nav__links-wrapper {
+    display: flex;
+    margin: 0;
+    justify-content: flex-end;
+    align-items: center;
   }
 
   .nav__links-list {
@@ -51,10 +54,72 @@
     max-width: 1200px;
   }
 
-  .nav__link--action {
+  .nav__action-btn {
+    flex-grow: 0;
+    color: #2275f4;
+    font-weight: 500;
     box-shadow: 0 2px 7px hsla(0, 0%, 0%, 0.5);
     background-color: #fff;
     margin-left: 20px;
+  }
+
+  .nav__action-btn:hover {
+    color: #75a8f6;
+  }
+
+  .nav__mobile-toggle {
+    display: none;
+    width: 32px;
+    height: 32px;
+    position: absolute;
+    top: -7px;
+    left: -5px;
+
+    cursor: pointer;
+
+    opacity: 0;
+    z-index: 2;
+  }
+
+  .nav__mobile-toggle-line {
+    display: none;
+    width: 15px;
+    height: 2px;
+    margin-bottom: 2px;
+    position: relative;
+    background: hsl(0, 0%, 66.7%);
+    border-radius: 3px;
+    z-index: 1;
+    transform-origin: 4px 0px;
+    transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
+      background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
+  }
+
+  .nav__mobile-toggle-line:first-child {
+    transform-origin: 0% 0%;
+  }
+
+  .nav__mobile-toggle-line:nth-last-child(2) {
+    transform-origin: 0% 100%;
+  }
+
+  .nav__mobile-toggle:checked ~ .nav__mobile-toggle-line {
+    opacity: 1;
+    transform: rotate(45deg) translate(-1px, 0px);
+    background: hsl(0, 0%, 66.7%);
+  }
+
+  .nav__mobile-toggle:checked ~ .nav__mobile-toggle-line:nth-last-child(3) {
+    opacity: 0;
+    transform: rotate(0deg) scale(0.2, 0.2);
+  }
+
+  .nav__mobile-toggle:checked ~ .nav__mobile-toggle-line:nth-last-child(2) {
+    transform: rotate(-45deg) translate(1px, -1px);
+  }
+
+  .nav__mobile-toggle:checked ~ .nav__links-list {
+    transform: none;
   }
 
   @media screen and (min-width: 992px) {
@@ -68,7 +133,8 @@
       padding: 8px 11px;
     }
 
-    .nav__link--action {
+    .nav__action-btn {
+      font-size: 14px;
       padding: 10px 23px;
     }
   }
@@ -84,7 +150,8 @@
       padding: 8px 5px;
     }
 
-    .nav__link--action {
+    .nav__action-btn {
+      font-size: 12px;
       padding: 10px 23px;
     }
   }
@@ -95,7 +162,7 @@
       padding-left: 10px;
     }
 
-    .nav__link--action {
+    .nav__action-btn {
       font-size: 12px;
       padding: 5px 18px;
       margin-left: 0;
@@ -103,6 +170,10 @@
 
     .nav {
       padding: 0;
+    }
+
+    .nav__links-wrapper {
+      height: 50px;
     }
 
     .nav__logo-img {
@@ -114,8 +185,46 @@
       margin-left: 20px;
     }
 
-    .nav__link:not(.nav__link--action) {
-      display: none;
+    .nav__mobile-menu {
+      position: fixed;
+      top: 20px;
+      left: 8px;
+      z-index: 2;
+      cursor: pointer;
+      user-select: none;
+    }
+
+    .nav__mobile-toggle {
+      display: block;
+    }
+
+    .nav__mobile-toggle-line {
+      display: block;
+    }
+
+    .nav__links-list {
+      position: absolute;
+      width: 300px;
+      margin: -100px 0 0 -50px;
+      padding: 50px 15px 50px 80px;
+      padding-top: 125px;
+      background: hsla(0, 0%, 100%, 0.95);
+      list-style-type: none;
+      border-right: 1px solid #e8e8e8;
+      min-height: calc(100vh + 100px);
+      transform-origin: 0 0;
+      transform: translate(-100%);
+      transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1);
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      flex-wrap: nowrap;
+    }
+
+    .nav__link-item {
+      padding: 10px 0;
+      font-size: 16px;
+      width: 100%;
     }
   }
 </style>
@@ -132,30 +241,33 @@
         </a>
       </div>
       <div class="col-6 col-sm-6 col-md-9">
-        <nav>
-          <ul class="nav__links-list">
-            <li class="nav__link-item">
-              <a class="nav__link">Features</a>
-            </li>
-            <li class="nav__link-item">
-              <a class="nav__link">Comparison</a>
-            </li>
-            <li class="nav__link-item">
-              <a class="nav__link">Documentation</a>
-            </li>
-            <li class="nav__link-item">
-              <a class="nav__link" href="pricing">Pricing</a>
-            </li>
-            <li class="nav__link-item">
-              <a class="nav__link">Open Sources</a>
-            </li>
-            <li class="nav__link-item">
-              <a class="nav__link nav__link--action">Talk to Us</a>
-            </li>
-          </ul>
+        <nav class="nav__links-wrapper">
+          <div class="nav__mobile-menu">
+            <input class="nav__mobile-toggle" type="checkbox" />
+            <span class="nav__mobile-toggle-line" />
+            <span class="nav__mobile-toggle-line" />
+            <span class="nav__mobile-toggle-line" />
+            <ul class="nav__links-list">
+              <li class="nav__link-item">
+                <a class="nav__link">Features</a>
+              </li>
+              <li class="nav__link-item">
+                <a class="nav__link">Comparison</a>
+              </li>
+              <li class="nav__link-item">
+                <a class="nav__link">Documentation</a>
+              </li>
+              <li class="nav__link-item">
+                <a class="nav__link" href="pricing">Pricing</a>
+              </li>
+              <li class="nav__link-item">
+                <a class="nav__link">Open Sources</a>
+              </li>
+            </ul>
+          </div>
+          <a class="nav__action-btn">Talk to Us</a>
         </nav>
       </div>
     </div>
   </div>
 </header>
-<MobileMenu />
